da4280b8f8ca20544454b8190e737f4766033b1f
Maximilian Strauss <straussmaximilian@googlemail.com>
2021-08-13 00:42:57 +0200

    FEAT GUI

--- README.md
+++ README.md
@@ -4,3 +4,24 @@ A leightweight task scheduling app build on streamlit.
 ## usecase
 
 For headless systems when being to lazy to write chronjobs.
+
+
+## Notes
+
+if a job takes longer than a scheduled interval, it will be skipped.
+
+
+## Ideas
+
+* Notification settings (Email, Slack, Teams)
+
+
+## Frequencies
+
+* Once
+* Interval
+- Minutes / Hours / Days / Weeks
+* TimePoint
+Date Picker 
+
+- Inital Execution
--- app.py
+++ app.py
@@ -5,23 +5,43 @@ import sqlite3
 import os
 import subprocess
 from functools import partial
-from datetime import datetime
+from datetime import datetime, timedelta
 import psutil
-import schedule
+from multiprocessing import Process
+import time
 
-#Creation date
-#Process id
-#String
-#Log direction
+FORMAT = {'created':[], 'process id' : [], 'job name': [], 'command': [], 'last execution': [], 'running': []}
 
-#Notification settings
-#Email
-#Teams / Slack notification
 
 def run_job(command, name, logfile):
     with open(f"./logs/{logfile} {name}_stdout.txt", "wb") as out, open(f"./logs/{logfile} {name}_stderr.txt", "wb") as err:
         p = subprocess.Popen(command.split(' '), stdout=out, stderr=err)
         return p
+
+def scheduler_process(command, name, logfile, start, timedelta, once):
+    while True:
+        now = datetime.now()
+        if now > (start + timedelta):
+            # Update database entry
+
+            p = run_job(command, name, logfile)
+            start += timedelta
+
+        if once:
+            break
+        else:
+            time.sleep(1)
+
+def run_process(command, job_name):
+    now = datetime.now()
+    now_str = now.strftime("%d_%m_%Y %H:%M:%S")
+    #FORMAT = {'created':[], 'process id' : [], 'job name': [], 'command': [], 'last execution': [], 'running': []}
+
+    p = Process(target=scheduler_process, args=(command, name, logfile, start, timedelta, once))
+    p.start()
+    df = pd.DataFrame({'created':[now], 'process id' : [p.pid], 'job name': [job_name], 'command': [command], 'last execution': [], 'running': []})
+    df.to_sql('processes', con=engine, if_exists='append', index=False)
+
 
 def write_st_log(file):
     with open(file, "r", encoding='utf-8') as reader:
@@ -58,15 +78,15 @@ st.text('A browser-based task scheduling system.')
 #last log
 try:
     df = pd.read_sql_table('processes', con=engine)
-    df['process running'] = df['process'].apply(lambda x: psutil.pid_exists(x))
+    df['running'] = df['process id'].apply(lambda x: psutil.pid_exists(x))
 except ValueError:
-    df = pd.DataFrame()
+    df = pd.DataFrame(FORMAT)
 
 st.table(df)
 
 
 if (len(df) > 0) and (df['process running']==False).sum() > 0:
-    if st.button('Remove unused processes.'):
+    if st.button('Remove processes that are not running.'):
         running = df[df['process running']]
         running.to_sql('processes', con=engine, if_exists='replace', index=False)
         raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
@@ -95,31 +115,46 @@ if (command != '') and st.button('Test command'):
             terminate_process(p.pid)
             break
 
-frequency = st.selectbox(
-    'Select Frequency',
-    ('Once','Daily', 'Weekly', 'Monthly'))
+col1, col2, col3 = st.beta_columns(3)
 
-if frequency != 'Once':
-    date = st.date_input('Starting date', datetime.now())
-    time = st.slider("Timepoint", min_value = datetime(2020, 1, 1, 00, 00), max_value = datetime(2020, 1, 1, 23, 59), value=datetime(2020, 1, 1, 12, 00), format="HH:mm")
+frequency = col1.selectbox(
+    'Select Frequency',
+    ('Once', 'Interval', 'Timepoint'))
+
+max_value_dict = {}
+max_value_dict['Minutes'] = 59
+max_value_dict['Hours'] = 59
+max_value_dict['Days'] = 364
+max_value_dict['Weeks'] = 51
+
+
+if frequency == 'Interval':
+    unit = col2.selectbox(
+        'Select Unit',
+        ('Minutes', 'Hours', 'Days', 'Weeks'))
+
+    quantity = col3.slider(f'Every x {unit}', min_value = 1, max_value = max_value_dict[unit])
+
+if frequency == 'Timepoint':
+    weekdays = col2.multiselect('Select weekdays:', options=['Mon','Tue','Wed','Thu','Fri','Sat','Sun'], default = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun'])
+
+c1, c2, c3 = st.beta_columns(3)
+
+execution = c1.selectbox(
+    'Execution',
+    ('Now','Scheduled'))
+
+if execution == 'Scheduled':
+    date = c2.date_input('Starting date', datetime.now())
+    time = c3.slider("Timepoint", min_value = datetime(2020, 1, 1, 00, 00), max_value = datetime(2020, 1, 1, 23, 59), value=datetime(2020, 1, 1, 12, 00), format="HH:mm")
+    td_ = time - datetime(2020, 1, 1, 00, 00)
+    start = date + td_
+
+    st.write(start)
 
 if st.button('Submit'):
-    now = datetime.now()
-    now_str = now.strftime("%d_%m_%Y %H:%M:%S")
-    p = run_job(command, job_name, now_str)
-    df = pd.DataFrame({'created':[now], 'process' : [p.pid], 'job_name': [job_name], 'command': [command]})
-    st.write(df)
-    df.to_sql('processes', con=engine, if_exists='append', index=False)
+    #p = run_process(command, job_name, start, timedelta)
     raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
 
-if False:
-    if len(logfiles) == 0:
-        st.info('No logfiles present.')
-        option = None
-    else:
-        option = st.selectbox('Read log', logfiles)
-
-    if option:
-        st.write(option)
-        write_st_log('./logs/'+option)
-        #write_st_log('stderr.txt')
+if st.button('Refresh'):
+    raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
