1b6560fd97f7559ab02ab5f37475fbac9fd76060
Maximilian Strauss <straussmaximilian@googlemail.com>
2021-09-18 19:30:14 +0200

    pypi action

---
+++ distribution/pypi/install_pypi_wheel.sh
@@ -0,0 +1,5 @@
+conda create -n tasklit_pip_test python=3.8 -y
+conda activate tasklit_pip_test
+pip install "tasklit"
+tasklit
+conda deactivate
---
+++ distribution/pypi/install_test_pypi_wheel.sh
@@ -0,0 +1,5 @@
+conda create -n tasklit_pip_test python=3.8 -y
+conda activate tasklit_pip_test
+pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple "tasklit"
+tasklit
+conda deactivate
