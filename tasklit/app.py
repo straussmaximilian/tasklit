e07c83bb3b11f955d60ee7d688d2b4b89df7ba30
Artem Vorobyev <artem.vorobyev.ev@gmail.com>
2021-09-19 18:34:52 +0200

    Fix homepage formatting

--- tasklit/app.py
+++ tasklit/app.py
@@ -2,15 +2,11 @@ import tasklit.settings.consts as settings
 import tasklit.src.utils.helpers as helper_functions
 
 from tasklit.pages.homepage import homepage
-from tasklit.src.classes import app_sqlite_handler
 
 
 # Create application resource folders, e.g. logs, data, etc.
 for folder in (settings.BASE_DATA_DIR, settings.BASE_LOG_DIR):
     helper_functions.create_folder_if_not_exists(folder)
-
-
-
 
 # Render application homepage
 homepage()
