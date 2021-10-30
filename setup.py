c2a61d9f906c1fd85508d83d7adeb0f4c5d908d5
Artem Vorobyev <artem.vorobyev.ev@gmail.com>
2021-10-30 19:58:33 +0200

    Remove unused import from setup.py

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-import tasklit
+"""Define PyPI setup."""
 
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 with open("requirements.txt") as requirements_file:
     requirements = [_ for _ in requirements_file]
 
 setup(
-    name='tasklit',
-    version='0.0.1',
-    url='https://github.com/straussmaximilian/tasklit',
-    author='Maximilian Strauss, Artem Vorobyev',
-    author_email='straussmaximilian@gmail.com',
-    description='A task scheduling app build on streamlit.',
+    name="tasklit",
+    version="0.0.1",
+    url="https://github.com/straussmaximilian/tasklit",
+    author="Maximilian Strauss, Artem Vorobyev",
+    author_email="straussmaximilian@gmail.com",
+    description="A task scheduling app build on streamlit.",
     packages=find_packages(),
     install_requires=requirements,
     entry_points={
