8b671cad5e1f14dc6c7afaad8fa3b659b7ddfd37
Artem Vorobyev <artem.vorobyev.ev@gmail.com>
2021-11-11 10:18:49 +0100

    more task_tracker tests

--- tests/test_stat_tracker.py
+++ tests/test_stat_tracker.py
@@ -1,9 +1,10 @@
 """Unittests for TaskStatisticsTracker."""
 
 import unittest
-from unittest.mock import MagicMock, patch
+from unittest.mock import MagicMock, call, patch
 
 import pandas as pd
+from pandas.util.testing import assert_frame_equal
 
 from tasklit.settings.consts import TaskInformation
 from tasklit.src.classes.stat_tracker import TaskStatisticsTracker
@@ -16,11 +17,14 @@ class TaskStatisticsTrackerTestCase(unittest.TestCase):
     def setUpClass(cls) -> None:
         """Test class parameters.
 
-        engine_uri (str): mock engine URI path.
-        table_name (str): sample table name.
-        tables (List[MagicMock]): list of mock objects representing
-            DB tables.
-        sql_repository (SQLDataRepository): instantiated sql repository object.
+        task: TaskInformation
+            sample task information.
+        empty_task_dataframe: pd.DataFrame
+            empty DF to test adding a new task row.
+        non_empty_task_dataframe: pd.DataFrame
+            non-empty DF to test updating a task row.
+        task_tracker: TaskStatisticsTracker
+            instantiated task tracker with an empty DF.
         """
         super(TaskStatisticsTrackerTestCase, cls).setUpClass()
         cls.task = TaskInformation("sample_job", "echo 'Max is awesome!'", 0.1)
@@ -36,5 +40,34 @@ class TaskStatisticsTrackerTestCase(unittest.TestCase):
             mock_db_handler.save_dataframe.return_value = True
             cls.task_tracker = TaskStatisticsTracker(cls.task)
 
-    def test_print_stats_df(self):
-        print(self.task_tracker._stats_df)
+    def test_load_stats_df(self):
+        """Unittest for TaskStatisticsTracker._load_stats_df().
+
+        GIVEN a table name
+        WHEN _load_stats_df() method is called
+        THEN check that correct dataframe output is returned.
+        """
+        assert_frame_equal(self.task_tracker._load_stats_df(), pd.DataFrame())
+
+        self.task_tracker._db_handler.assert_has_calls(
+            [
+                call.load_dataframe("process_stats"),
+            ]
+        )
+
+    def test_save_stats_df_append(self):
+        """Unittest for TaskStatisticsTracker._save_stats_df().
+
+        GIVEN a dataframe with task information
+        WHEN _save_stats_df() method is called
+        THEN check that related database handler method is called.
+        """
+        expected_call = ("process_stats", {"if_exists": "replace"})
+        self.task_tracker._save_stats_df(self.empty_task_dataframe)
+
+        df_call = self.task_tracker._db_handler.save_dataframe.call_args_list[
+            0
+        ]
+
+        assert_frame_equal(df_call[0][0], pd.DataFrame())
+        self.assertEqual(expected_call, (df_call[0][1], df_call[1]))
